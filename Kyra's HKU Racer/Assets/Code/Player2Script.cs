592876db70db329858c4ddb6a324408ae5f24ec4
Kyrakiweh <36536464+Kyrakiweh@users.noreply.github.com>
2018-06-23 08:55:11 +0200

    Controller player 2 en machines
    Vliegmachines geïmporteerd en de controller voor speler 2 aan de praat gekregen

--- Kyra's HKU Racer/Assets/Code/Player2Script.cs
+++ Kyra's HKU Racer/Assets/Code/Player2Script.cs
@@ -2,31 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Script : MonoBehaviour {
+public class Player2Script : MonoBehaviour {
 
     public float speed = 10.0f;
 
     // Use this for initialization
-     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update () {
-
-        float translation = Input.GetAxis("Vertical") * speed;
-        float strafe = Input.GetAxis("Horizontal") * speed;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical2") * speed;
+        float strafe = Input.GetAxis("Horizontal2") * speed;
         translation *= Time.deltaTime;
         strafe *= Time.deltaTime;
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, strafe, 0);
-
-
-
-
-
-	}
-    
+    }
 }
