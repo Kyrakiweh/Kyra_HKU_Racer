81a56047558da5a8834d0533ff641a49d9d6a38c
Kyrakiweh <36536464+Kyrakiweh@users.noreply.github.com>
2018-06-29 16:52:08 +0200

    Triggers are working now
    De triggers werken en ik kan tellen hoeveel rondjes mijn voertuigen hebben gereden , nu aan de slag om er ook een te laten winnen

---
+++ Kyra's HKU Racer/Assets/Code/CompleteLapTrigScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompleteLapTrigScript : MonoBehaviour {
+
+    public GameObject LapCompleteTrigger;
+    public GameObject HalfLapTrigger;
+
+    public static int lapNr;
+    public GameObject LapNumberDisplay;
+
+    public static int lapNr2;
+    public GameObject LapNumberDisplay2;
+
+   
+
+    private void Start()
+    {
+        lapNr = 1;
+        lapNr2 = 1; 
+        LapNumberDisplay.GetComponent<Text>().text = "1";
+        LapNumberDisplay2.GetComponent<Text>().text = "1";
+
+    }
+
+    void OnTriggerEnter(Collider  collider) // ff kijken
+    {
+        if (collider.gameObject.name == "Player1") 
+        {
+            lapNr += 1;
+            LapNumberDisplay.GetComponent<Text>().text = "" + lapNr;
+
+            if (lapNr >= 2)
+            {
+
+            }
+            
+         }
+        
+
+        if (collider.gameObject.name == "Player2")
+        {
+            lapNr2 += 1;
+            Debug.Log("speler2 lap");
+            LapNumberDisplay2.GetComponent<Text>().text = "" + lapNr2;
+         }
+
+
+
+        
+    }
+}
