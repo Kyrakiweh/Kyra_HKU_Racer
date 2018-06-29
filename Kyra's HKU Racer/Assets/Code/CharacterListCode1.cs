d537ab1862ee87d8a385b5873d685988f6f603f7
Kyrakiweh <36536464+Kyrakiweh@users.noreply.github.com>
2018-06-29 22:04:02 +0200

    UI, CharacterSelection
    Ik kan de characters kiezen en selecteren

---
+++ Kyra's HKU Racer/Assets/Code/CharacterListCode1.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterListCode1 : MonoBehaviour {
+
+    private GameObject[] characterList;
+    private int index=0;
+
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterList = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+
+            characterList[i] = transform.GetChild(i).gameObject;
+        foreach (GameObject go in characterList)
+        {
+            go.SetActive(false);
+        }
+
+        if (characterList[index])
+        {
+            characterList[index].SetActive(true);
+        }
+    }
+        public void ToggleLeft()
+        {
+        //toggle off the current model
+        characterList[index].SetActive(false);
+
+            index -= 1;
+            if (index < 0)
+        {
+            index = characterList.Length - 1;
+        }
+        //toggle on the next model
+        characterList[index].SetActive(true);
+    }
+
+
+    public void ToggleRightt()
+    {
+        //toggle off the current model
+        characterList[index].SetActive(false);
+
+        index += 1;
+        if (index == characterList.Length )
+        {
+            index = 0;
+        }
+        //toggle on the next model
+        characterList[index].SetActive(true);
+    }
+
+    public void StartButton()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene("Scene Racer");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
